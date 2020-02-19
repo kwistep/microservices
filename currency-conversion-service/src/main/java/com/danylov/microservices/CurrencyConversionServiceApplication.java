d5e729e338a8bb98f67afcde9c8511c160545cd7
Anton Danylov <ur.anton.danilov@gmail.com>
2020-02-18 19:57:47 -0800

    Fix currency-conversion-service to start

--- currency-conversion-service/src/main/java/com/danylov/microservices/CurrencyConversionServiceApplication.java
+++ currency-conversion-service/src/main/java/com/danylov/microservices/CurrencyConversionServiceApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients("com.danylov.microservices")
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 public class CurrencyConversionServiceApplication {
 
     public static void main(String[] args) {
