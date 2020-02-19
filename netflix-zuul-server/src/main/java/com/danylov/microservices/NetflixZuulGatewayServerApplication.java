5bd5a02b4652e6d3f48f57e53ddb4f2709bcc529
Anton Danylov <ur.anton.danilov@gmail.com>
2020-02-18 18:45:23 -0800

    Initial commit

---
+++ netflix-zuul-server/src/main/java/com/danylov/microservices/NetflixZuulGatewayServerApplication.java
@@ -0,0 +1,13 @@
+package com.danylov.microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetflixZuulGatewayServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NetflixZuulGatewayServerApplication.class, args);
+    }
+
+}
